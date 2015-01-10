aff5c721786d5a08595b468bc9a87c164e6d796c
Sourec <tothemyers@gmail.com>
2015-01-10 08:47:18 -0500

    Added core functionality for test robot
    -Added core functionality for a simple test robot that uses a talon to
    move a motor based on the input from one axis of a joystick.
    -Attempted and failed to disable the watchdog
    -For the moment, for pure early testing purposes, MecanumDrive is
    disabled. This is not permanent.

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,19 +1,24 @@
 #include "WPILib.h"
-#include "MecanumDrive.h"
+//#include "MecanumDrive.h"
 
 
 class Robot: public IterativeRobot {
 private:
 	LiveWindow *lw;
-	MecanumDrive *drivebase;
+	//MecanumDrive *drivebase;
+	Talon* testMotor = NULL;
+	Joystick* controller;
 
 public:
 	void RobotInit() {
 		lw = LiveWindow::GetInstance();
+		testMotor = new Talon(1);
+		controller = new Joystick(1);
+		//&GetWatchdog()->SetEnabled(false);
 	}
 
 	void AutonomousInit() {
-		drivebase->EnableControl();
+		//drivebase->EnableControl();
 	}
 
 	void AutonomousPeriodic() {
@@ -21,15 +26,15 @@ public:
 	}
 
 	void TeleopInit() {
-		drivebase->DisableControl();
+		//drivebase->DisableControl();
 	}
 
 	void TeleopPeriodic() {
-
+		testMotor->Set(controller->GetRawAxis(1));
 	}
 
 	void TestPeriodic() {
-		lw->Run();
+		//lw->Run();
 	}
 };
 
