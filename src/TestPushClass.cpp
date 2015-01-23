8a7a5b774b30d5f1047b662917d1e3e32cb2b1af
johnnyensign <ensign.johnpaul+git@gmail.com>
2015-01-23 17:34:28 -0500

    Testing EGit with Eclipse to pull/push files to github

---
+++ src/TestPushClass.cpp
@@ -0,0 +1,18 @@
+/*
+ * TestPushClass.cpp
+ *
+ *  Created on: Jan 23, 2015
+ *      Author: jpme
+ */
+
+#include <TestPushClass.h>
+
+TestPushClass::TestPushClass() {
+	// TODO Auto-generated constructor stub
+
+}
+
+TestPushClass::~TestPushClass() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/TestPushClass.h
@@ -0,0 +1,17 @@
+/*
+ * TestPushClass.h
+ *
+ *  Created on: Jan 23, 2015
+ *      Author: jpme
+ */
+
+#ifndef SRC_TESTPUSHCLASS_H_
+#define SRC_TESTPUSHCLASS_H_
+
+class TestPushClass {
+public:
+	TestPushClass();
+	virtual ~TestPushClass();
+};
+
+#endif /* SRC_TESTPUSHCLASS_H_ */
