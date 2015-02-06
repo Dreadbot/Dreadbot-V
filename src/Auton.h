61203a7bb71720f7bfb0d4d8549b33e0f976a837
Parker Stebbins <SuperAlpacaMan@users.noreply.github.com>
2015-02-06 08:44:00 -0500

    Partially implemented

--- src/Auton.h
+++ src/Auton.h
@@ -3,4 +3,35 @@
 #include "WPILib.h"
 
 namespace dreadbot {
+  struct Goal {
+    
+  }
+  class RobotState { // Describes a goal state for the robot
+  public:
+    RobotState(IterativeRobot* _robot, Goal* goal) {
+      robot = _robot;
+    }
+    
+    bool Accomplished(); // Returns true when the goal is accomplished
+    void ExecuteAndYield(); // Pause execution until the goal is accomplished
+    
+  private:
+    IterativeRobot* robot;
+    Goal* goal;
+  }
+  
+  class Auton {
+  public:
+    Auton(MecanumDrive* _drivebase, RPS* _tracker) {
+      drivebase = _drivebase;
+      tracker = _tracker;
+    }
+    
+  private:
+    MecanumDrive* drivebase;
+    RPS* tracker;
+    
+    float TimeCheck(); // Return time until match ends
+    
+  }
 }
