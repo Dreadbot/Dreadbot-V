bf24be9deb0d5ed7d9e0de25e03a1db59abcf62f
Sourec <tothemyers@gmail.com>
2015-02-06 08:02:00 -0500

    Temporarily commented out some vision stuff, changed controllerID from 0 to 1 for drivebase controls only

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -12,14 +12,14 @@ namespace dreadbot {
 	private:
 		DriverStation *ds;
 		LiveWindow *lw;
-		Joystick* gamepad;
+		//Joystick* gamepad;
 		Input::XMLInput* Input;
 		PowerDistributionPanel *pdp;
 		MecanumDrive *drivebase;
 
-		AxisCamera* frontCamera;
-		AxisCamera* backCamera;
-		bool viewingBack;
+		//AxisCamera* frontCamera;
+		//AxisCamera* backCamera;
+		//bool viewingBack;
 
 	public:
 		void RobotInit() {
@@ -32,9 +32,9 @@ namespace dreadbot {
 			Input->setDrivebase(drivebase);
 
 			//Vision stuff
-			frontCamera = new AxisCamera("10.36.56.11");
-			backCamera = new AxisCamera("10.0.0.11");
-			viewingBack = false;
+			//frontCamera = new AxisCamera("10.36.56.11");
+			//backCamera = new AxisCamera("10.0.0.11");
+			//viewingBack = false;
 
 		}
 
@@ -47,31 +47,32 @@ namespace dreadbot {
 
 		void TeleopInit() {
 			Input->loadXMLConfig("/XML Bot Config.xml");
-			gamepad = Input->getController(1);
+			//gamepad = Input->getController(1);
 			drivebase->Engage();
 		}
 
 		void TeleopPeriodic() {
 			drivebase->SD_RetrievePID();
 			Input->updateDrivebase();
-			Input->updateInds();
+			drivebase->SD_OutputDiagnostics();
+			//Input->updateInds();
 
 			//Process control switching
 
-			if (gamepad->GetRawAxis(CAMSWITCH_AXIS) > 0) //Switch to front
-				viewingBack = false;
-			else if (gamepad->GetRawAxis(CAMSWITCH_AXIS) < 0)
-				viewingBack = false;
-
-			//Apply the image to the camera server
-			Image* image = NULL;
-			image = imaqCreateImage(IMAQ_IMAGE_RGB, 0);
-			if (viewingBack) //If the back camera is the currently active view...
-				backCamera->GetImage(image);
-			else
-				frontCamera->GetImage(image);
-			CameraServer::GetInstance()->SetImage(image);
-			delete image;
+//			if (gamepad->GetRawAxis(CAMSWITCH_AXIS) > 0) //Switch to front
+//				viewingBack = false;
+//			else if (gamepad->GetRawAxis(CAMSWITCH_AXIS) < 0)
+//				viewingBack = false;
+//
+//			//Apply the image to the camera server
+//			Image* image = NULL;
+//			image = imaqCreateImage(IMAQ_IMAGE_RGB, 0);
+//			if (viewingBack) //If the back camera is the currently active view...
+//				backCamera->GetImage(image);
+//			else
+//				frontCamera->GetImage(image);
+//			CameraServer::GetInstance()->SetImage(image);
+//			delete image;
 		}
 
 		void TestPeriodic() {
