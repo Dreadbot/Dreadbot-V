783af1c339046b4dc2a8cd069f6520cf091c71af
Sourec <tothemyers@gmail.com>
2015-02-12 19:58:24 -0500

    Half-work on RobotFSM

--- src/Autonomous.cpp
+++ src/Autonomous.cpp
@@ -2,5 +2,31 @@
 
 namespace dreadbot
 {
-	
+	RobotFSM::RobotFSM()
+	{
+		drivebase = null;
+	}
+	void RobotFSM::update()
+	{
+		if (state == stopped)
+		{
+			globalTimer.Start();
+			state = getting_tote;
+		}
+
+		if (state == getting_tote)
+		{
+			//Has the tote already been picked up?
+			//if (globalTimer.HasPeriodPassed(TOTE_PICKUP_TIME))
+				//switchState(drive_to_zone);
+
+			drivebase->Drive_v(0, .75, 0); //Drive forward at 3/4 speed
+
+		}
+
+		else if (state == drive_to_zone)
+		{
+
+		}
+	}
 };
--- src/Autonomous.h
+++ src/Autonomous.h
@@ -1,19 +1,32 @@
 #pragma once
 
 #include "WPILib.h"
+#include "Timer.h"
 #include "MecanumDrive.h"
+#include "XMLInput.h"
+
+#define TOTE_PICKUP_TIME 2500
+#define DRIVE_TO_ZONE_TIME 3000
+#define null 0
 
 namespace dreadbot 
 {
-	enum robotState{};
+	enum robotState{start, stopped, getting_tote, drive_to_zone};
 
 	class RobotFSM
 	{
 	public:
-		RobotSFM(MecanumDrive* base);
-		~RobotSFM();
-		void update()
+		RobotFSM();
+		~RobotFSM();
+		void SetHardware(MecanumDrive* base, MotorGrouping* newIntake, MotorGrouping* newTransit);
+		void update();
+		void switchState(robotState newState);
 	protected:
 		MecanumDrive* drivebase;
+		Timer globalTimer;
+		robotState state;
+
+		MotorGrouping* intake;
+		MotorGrouping* transit;
 	};
 };
