b271574e26a3ad823cb4eec42cfefa773c88b04c
Sourec <tothemyers@gmail.com>
2015-02-22 22:05:26 -0500

    Very early implementation of HALBot and associated states.
    Don't expect it to compile (it's just a header, after all!), and it is VERY incomplete! There are going to be a lot of changes soon, I just ran out of time to make them tonight.

--- src/Autonomous.h
+++ src/Autonomous.h
@@ -14,5 +14,46 @@
 
 namespace dreadbot 
 {
+	//States
+	class GettingTote : public FSMState
+	{
+	public:
+		int update();
+		Timer getTimer;
+	};
+	class DriveToZone : public FSMState
+	{
+	public:
+		int update();
+	private:
+		Timer driveTimer;
+	};
+	class Stopped : public FSMState
+	{
+	public:
+		int update();
+	};
+
+	class HALBot
+	{
+	public:
+		enum fsmInputs {timerExpired, sensorHit};
+
+		HALBot(MecanumDrive* newDrivebase, MotorGroup* newIntake)
+		void update();
+		void start();
+		static void Drive(int x, int y, int r);
+		static void Intake(int speed);
+		static void startDriveTimer();
+		static void startGetTimer();
+	private:
+		FiniteStateMachine* FSM;
+
+		MecanumDrive* drivebase;
+		MotorGroup* intake;
+
+		FSMTransition[] transitionTable;
+	};
+
 	float getParallelTurnDir(Ultrasonic* frontUltra, Ultrasonic* rearUltra);
 }
