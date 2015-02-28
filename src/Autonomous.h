4c5f74dd12ec757d071d80abc945e5d3d2be18e3
Sourec <tothemyers@gmail.com>
2015-02-28 14:51:26 -0500

    Increased rotate and drive time

--- src/Autonomous.h
+++ src/Autonomous.h
@@ -8,8 +8,8 @@
 #include "FSM.h"
 
 #define TOTE_PICKUP_TIME 1
-#define DRIVE_TO_ZONE_TIME 3
-#define ROTATE_TIME 0.5
+#define DRIVE_TO_ZONE_TIME 5
+#define ROTATE_TIME 0.66
 #define DIST_FROM_WALL 2000 //Millimeters!
 #define ULTRASONIC_SEPARATION 750 //Also millimeters!
 
