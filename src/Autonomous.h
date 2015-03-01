2ec360e06ef939a836381ee779b2a7de09386489
Sourec <tothemyers@gmail.com>
2015-02-28 21:06:24 -0500

    Calibration on DriveToZone time

--- src/Autonomous.h
+++ src/Autonomous.h
@@ -8,7 +8,7 @@
 #include "FSM.h"
 
 #define TOTE_PICKUP_TIME 1
-#define DRIVE_TO_ZONE_TIME 5
+#define DRIVE_TO_ZONE_TIME 4.1
 #define ROTATE_TIME 0.66
 #define DIST_FROM_WALL 2000 //Millimeters!
 #define ULTRASONIC_SEPARATION 750 //Also millimeters!
