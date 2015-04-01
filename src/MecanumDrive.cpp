77d063e943bd2225446367531c97967ecca41892
Parker Stebbins <SuperAlpacaMan@users.noreply.github.com>
2015-04-01 13:01:02 -0400

    Update MecanumDrive.cpp

--- src/MecanumDrive.cpp
+++ src/MecanumDrive.cpp
@@ -29,10 +29,6 @@ void MecanumDrive::Set(int motorId_lf, int motorId_rf, int motorId_lr, int motor
 		motors[i]->SetPID(0.5, 0, 0, 0);
 		motors[i]->SetVoltageRampRate(0.5f); //Ramp up for drive motors
 	}
-
-	//x_ctrl = new SimplePID(0.2, 0, 0, false);
-	//y_ctrl = new SimplePID(0.2, 0, 0, false);
-	//r_ctrl = new SimplePID(0.2, 0, 0, true);
 }
 
 // Constructor
@@ -44,7 +40,6 @@ MecanumDrive::MecanumDrive(int motorId_lf, int motorId_rf, int motorId_lr, int m
 MecanumDrive::~MecanumDrive() {
 	Disengage();
 	for (uint8_t i = 0; i < MOTOR_COUNT; ++i) {
-
 		delete motors[i];
 	}
 }
@@ -61,29 +56,10 @@ void MecanumDrive::GoSlow() {
 	}
 }
 
-// Drive to position
-void MecanumDrive::Drive_p(double x, double y, double rotation) {
-	if (mode == drivemode::absolute) {
-		//vec_out.rotate((double) gyroscope->GetAngle());
-		// yield thread until the robot has passed within a certain tolerance of the target?
-		// a callback might work?
-	}
-}
-
 // Drive with wheel velocity
 void MecanumDrive::Drive_v(double x, double y, double rotation) {
 	Vector2<double> vec_out(y, x);
 	double rot_out = -rotation - (sqrt(x*x + y*y) * 0.01);
-
-	if (mode == drivemode::relative) {
-		/*
-		#ifdef SQUARE_INPUTS
-			vec_out.x = vec_out.x*std::abs(vec_out.x);
-			vec_out.y = vec_out.y*std::abs(vec_out.y);
-			rot_out = rot_out*std::abs(rot_out);
-		#endif
-		*/
-	}
 
 	double wspeeds[4];
 	wspeeds[m_leftFront] = vec_out.x + vec_out.y + rot_out;
@@ -101,27 +77,9 @@ void MecanumDrive::Drive_v(double x, double y, double rotation) {
 			wspeeds[i] /= maxMagnitude;
 		}
 	}
-
-	bool stall = true;
+	
 	for (uint8_t i = 0; i < MOTOR_COUNT; ++i) {
-		// files.andymark.com/CIM-motor-curve.pdf
-		stall = stall && (motors[i]->GetOutputCurrent() > STALL_MOTOR_CURRENT);
-	}
-	for (uint8_t i = 0; i < MOTOR_COUNT; ++i) {
-		motors[i]->Set(wspeeds[i]*motorReversals[i]*SmartDashboard::GetNumber("Speed", 1000.0), syncGroup); // *stall
-	}
-}
-
-void MecanumDrive::SetDriveMode(drivemode newMode) {
-	mode = newMode;
-	CANSpeedController::ControlMode cmode;
-	if (mode == drivemode::relative) {
-		cmode = CANSpeedController::ControlMode::kSpeed;
-	} else if (mode == drivemode::absolute) {
-		cmode = CANSpeedController::ControlMode::kPosition;
-	}
-	for (uint8_t i = 0; i < MOTOR_COUNT; ++i) {
-		motors[i]->SetControlMode(cmode);
+		motors[i]->Set(wspeeds[i]*motorReversals[i]*1023, syncGroup);
 	}
 }
 
