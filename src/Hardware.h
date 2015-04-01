9a50c8bf746ce74c06dfa2861b9d63065aba222f
Parker Stebbins <SuperAlpacaMan@users.noreply.github.com>
2015-04-01 08:11:09 -0400

    Update Hardware.h

--- src/Hardware.h
+++ src/Hardware.h
@@ -3,18 +3,18 @@
 #include "WPILib.h"
 #include "XMLInput.h"
 
-#define drivebase   Hardware::drivebase
-#define lift        Hardware::lift
-#define fork        Hardware::fork
-#define intake      Hardware::intake
-#define intakeArms  Hardware::intakeArms
+#define drivebase	Hardware::drivebase
+#define lift		Hardware::lift
+#define liftArms	Hardware::fork
+#define intake		Hardware::intake
+#define intakeArm	Hardware::intakeArms
 
 namespace dreadbot {
-  struct Hardware {
-    static MecanumDrive* drivebase;
-	  static MotorGrouping* intake;
+	struct Hardware {
+		static MecanumDrive* drivebase;
+		static MotorGrouping* intake;
 		static PneumaticGrouping* lift;
 		static PneumaticGrouping* fork;
 		static PneumaticGrouping* intakeArms;
-  };
+	};
 }
