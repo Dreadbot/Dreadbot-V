f8c87ddacd47a796d0eaf174cc7f9a2db2466c08
Sourec <tothemyers@gmail.com>
2015-04-23 12:09:18 -0400

    Switched autotote control to start button

--- src/Robot.h
+++ src/Robot.h
@@ -13,19 +13,19 @@
 	#define AXS_DRIVE_X 0
 	#define AXS_DRIVE_Y 1
 	#define AXS_DRIVE_R 4
-  	#define AXS_LIFT_DOWN 2 // LB
- 	#define AXS_INTAKE_IN 3 // RB
-	#define BTN_ARMS_OUT  6 // RT
- 	#define BTN_OPEN_FORK 5 // LT
-	#define BTN_STOP_LIFT 1 // X
+  	#define AXS_LIFT_DOWN 2 //LB
+ 	#define AXS_INTAKE_IN 3 //RB
+	#define BTN_ARMS_OUT  6 //RT
+ 	#define BTN_OPEN_FORK 5 //LT
+	#define BTN_STOP_LIFT 1 //X
 	// Backup driver controls
 	#define B_AXS_TOTE_OUT 3	//RB
 	#define B_AXS_TOTE_IN 2		//LB
 	#define B_BTN_ARMS_OUT 3	
 	#define B_BTN_ARMS_IN 2		
-	#define B_BTN_AUTOTOTE 8
-	//#define B_BTN_GEDDAN 5 // Robot does a short dance to recover from an intake failure
+	#define B_BTN_AUTOTOTE 7 //Back
+	//#define B_BTN_GEDDAN 5 //Robot does a short dance to recover from an intake failure
 
 	// Shared controls
-	#define BTN_SWITCH_CAM 8 // Camera change is start
+	#define BTN_SWITCH_CAM 8 //Camera change is start
 #endif
