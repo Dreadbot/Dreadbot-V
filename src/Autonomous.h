b0b9290315165bf39ae61f2b614557777b4f1998
Parker <parkerstebbins@gmail.com>
2015-04-24 11:37:47 -0400

    Timing tweaks - mostly working

--- src/Autonomous.h
+++ src/Autonomous.h
@@ -10,8 +10,6 @@
 #include "../lib/Logger.h"
 using namespace Hydra;
 
-// Autonomous operational parameters
-
 #define ESTOP_TIME 				5.0f 	// How long the robot will wait without getting a tote until it e-stops
 #define STRAFE_TO_ZONE_TIME 	3.1f 	// Used in some auton modes (!2TA !3TA) - how long does the robot strafe?
 #define DRIVE_TO_ZONE_TIME 		2.0f 	// How long the robot normally drives forward
@@ -20,49 +18,24 @@ using namespace Hydra;
 #define INTAKE_PUSH_SPEED		1.0f	// Rotational velocity of the intake wheels when pushing a container out of the way
 #define PUSH_TIME 				0.9f 	// How long the robot pushes containers out of the way
 #define PUSH_SPEED 				0.75f 	// How fast the robot drives forward when pushing containers
-#define DRIVE_STRAFE_CORRECTION	0.0f 	// Aport drift abbertaion correctional factor
+#define DRIVE_STRAFE_CORRECTION	0.04f 	// Aport drift abbertaion correctional factor
 #define DRIVE_ROTATE_CORRECTION	0.0f 	// Rotate abberation correctional factor
 
 // Endgame parameters
 #define RD_DRIVE_SPEED 			1.0f 	// Astern speed in RotateDrive.
-#define RD_ROTATE_SPEED 		-0.6f 	// Size of the RotateDrive arc
-#define BACK_AWAY_TIME 			0.75f 	// How long the robot backs up after stopping in 3TA
-#define ROTATE_TIME 			1.16f 	// Also, timing is modified in RotateDrive::update - 0.5 s is subtracted
-#define ROTATE_DRIVE_STRAIGHT 	0.5f 	// How long to drive straight in RotateDrive AFTER rotating
+#define RD_ROTATE_SPEED 		-0.4f 	// Size of the RotateDrive arc
+#define BACK_AWAY_TIME 			0.6f 	// How long the robot backs up after stopping in 3TA
+#define ROTATE_TIME 			0.8f 	// Also, timing is modified in RotateDrive::update - 1.0 s is subtracted
+#define ROTATE_DRIVE_STRAIGHT 	0.2f 	// How long to drive straight in RotateDrive AFTER rotating
 
 // Tote collection parameters
 #define STACK_CORRECTION_TIME 	0.25f 	// How long the robot jerks backward
-#define STACK_CORRECTION_SPEED 	0.9f 	// How quickly the robot jerks backward
+#define STACK_CORRECTION_SPEED 	0.85f 	// How quickly the robot jerks backward
 #define LIFT_ENGAGEMENT_DELAY 	0.5f 	// How long the robot waits after starting to lower the lift while collecting the third tote before reversing.
-
 
 
 namespace dreadbot 
 {
-	#define STRAFE_TO_ZONE_TIME = SmartDashboard::GetNumber('STRAFE_TO_ZONE_TIME', 3.1f 	// Used in some auton modes (!2TA !3TA) - how long does the robot strafe?
-	#define DRIVE_TO_ZONE_TIME = SmartDashboard::GetNumber('DRIVE_TO_ZONE_TIME', 2.0f 	// How long the robot normally drives forward
-
-	// Container evacuation parameter
-	#define INTAKE_PUSH_SPEED = SmartDashboard::GetNumber('		', 0.95f);	// Rotational velocity of the intake wheels when pushing a container out of the way
-	#define PUSH_TIME = SmartDashboard::GetNumber(' 				', 0.9f); 	// How long the robot pushes containers out of the way
-	#define PUSH_SPEED = SmartDashboard::GetNumber(' 				', 0.75f); 	// How fast the robot drives forward when pushing containers
-	#define DRIVE_STRAFE_CORRECTION = SmartDashboard::GetNumber('', 0.0f); 	// Aport drift abbertaion correctional factor
-	#define DRIVE_ROTATE_CORRECTION	0.0f 	// Rotate abberation correctional factor
-
-	// Endgame parameters
-	#define RD_DRIVE_SPEED = SmartDashboard::GetNumber('', 0.8f); 	// Astern speed in RotateDrive.
-	#define RD_ROTATE_SPEED 		-0.6f 	// Size of the RotateDrive arc
-
-	#define BACK_AWAY_TIME 			0.6f 	// How long the robot backs up after stopping in 3TA
-	#define ROTATE_TIME 			1.16f 	// Also, timing is modified in RotateDrive::update - 1.0 s is subtracted
-	#define ROTATE_DRIVE_STRAIGHT 	0.5f 	// How long to drive straight in RotateDrive AFTER rotating
-
-	// Tote collection parameters
-	#define STACK_CORRECTION_TIME 	0.25f 	// How long the robot jerks backward
-	#define STACK_CORRECTION_SPEED 	0.9f 	// How quickly the robot jerks backward
-	#define LIFT_ENGAGEMENT_DELAY 	0.5f 	// How long the robot waits after starting to lower the lift while collecting the third tote before reversing.
-
-
 	//States
 	class GettingTote : public FSMState
 	{
